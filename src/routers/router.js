589c341a9f06a2a8f9b36bb711d902344f427a10
Jose Jeferson Lima Feitosa <jjefersonlimaf@gmail.com>
2023-12-12 00:19:29 -0300

    refeito

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const verifyLogin = require('../middlewares.js/authorization');
 const listCategories = require('../controller/categories');
-const { registerUser, loginUser } = require('../controller/users');
+const usuarios = require('../controller/users');
 const validateReqBody = require('../middlewares.js/validateReqBody');
 const schemaEmailAndPassword = require('../schemas/schemaEmailAndPassword');
 const schemaUser = require('../schemas/schemaUsers');
@@ -13,9 +13,8 @@ router.use(express.json());
 
 
 router.get('/categoria', listCategories)
-router.post('/usuarios', validateReqBody(schemaUser), registerUser);
-router.post('/login', validateReqBody(schemaEmailAndPassword), loginUser);
-
+router.post('/usuarios', validateReqBody(schemaUser), usuarios.registerUser);  
+router.post('/login', validateReqBody(schemaEmailAndPassword), usuarios.loginUser);
 router.use(verifyLogin);
 
 
